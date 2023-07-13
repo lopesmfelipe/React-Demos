953be5383c7bfed63dd330390062996885fa3550
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-07-13 01:05:08 -0400

    Project created

---
+++ src/main/java/com/company/gameslist/GameslistApplication.java
@@ -0,0 +1,13 @@
+package com.company.gameslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/gameslist/GameslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.gameslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
