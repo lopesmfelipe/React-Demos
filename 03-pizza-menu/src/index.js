c98dce1bd89ef811b73d851167e40f2b794cb3fb
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-24 12:17:41 -0400

    update

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return <h1> This website is built using React JS!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
