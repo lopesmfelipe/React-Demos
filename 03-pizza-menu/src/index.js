6462a0763a30d110de92b93bf80be52c7e4f41bc
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-27 08:50:39 -0400

    Verifying if there is pizzas

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -69,15 +69,21 @@ function Header() {
 }
 
 function Menu() {
+   const pizzas = pizzaData;
+  //const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name}/>
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/*<Pizza
         name="Pizza Spinaci"
@@ -112,7 +118,7 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 2;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -120,7 +126,14 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        {new Date().toLocaleTimeString()}. We're currently open
+        {isOpen && (
+          <div className="order">
+            <p>
+              We're open until {closeHour}:00. Come visit us or order online
+            </p>
+            <button className="btn"> Order </button>
+          </div>
+        )}
       </footer>
     </>
   );
