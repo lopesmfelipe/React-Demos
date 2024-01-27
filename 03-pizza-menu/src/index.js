3002eae91171a6970e19801562db46d93d03382b
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-27 09:03:35 -0400

    Ran out of pizza result

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -69,21 +69,21 @@ function Header() {
 }
 
 function Menu() {
-   const pizzas = pizzaData;
-  //const pizzas = [];
+   //const pizzas = pizzaData;
+  const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
-      )}
+      ) : <h1> Sorry but we ran out of pizza. Please come back later :)</h1>}
 
       {/*<Pizza
         name="Pizza Spinaci"
