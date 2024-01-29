d22d97a621e3baa1e181be696fde5a6865963337
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-01-29 14:33:13 -0400

    Step state

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,9 +1,55 @@
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 💷",
+];
 
 function App() {
+  const [step, setStep] = useState(1);
+
+
+  function handlePrevious() {
+    setStep(step > 1 ? step - 1 : step);
+  }
+  
+  function handleNext() {
+    setStep(step < 3 ? step + 1 : step);
+  }
+
   return (
-    <div className="App">
-      <h1> The world is yours</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}> 1 </div>
+        <div className={step >= 2 ? "active" : ""}> 2 </div>
+        <div className={step >= 3 ? "active" : ""}> 3 </div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
