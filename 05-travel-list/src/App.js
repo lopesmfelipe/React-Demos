4c06b31d9f91c5521e94b58e7794a97d35e6a3a5
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-02-04 09:03:20 -0400

    Items list

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 1, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -41,7 +42,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
