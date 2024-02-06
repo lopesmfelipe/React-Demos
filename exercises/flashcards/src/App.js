d9975d9a1ada75b1fc2fbfa561e2bdcaf8a67f60
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2024-02-06 14:56:21 -0400

    Flashcard dynamically

--- exercises/flashcards/src/App.js
+++ exercises/flashcards/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const qa = [
+    {
+      id: 3457,
+      question: "What language is React based on?",
+      answer: "JavaScript",
+    },
+    {
+      id: 7494,
+      question: "How to pass data from parent to child components?",
+      answer: "With props",
+    },
+    {
+      id: 2311,
+      question: "What are the building blocks of React apps?",
+      answer: "Components",
+    },
+    { 
+      id: 2341,
+      question: "How to give components memory",
+      answer: "With state" },
+    {
+      id: 9844,
+      question: "What's hte name of the syntax we use to describe a UI in React",
+      answer: "JSX",
+    },
+    {
+      id: 5648,
+      question:
+      "What do we call an input element that is completely synchronised with state?",
+      answer: "A controlled element",
+    },
+  ];
+
+  function Flashcards({ question, answer }) {
+    const [turned, setTurned] = useState(false);
+
+    const cardStyle = {
+      backgroundColor: turned ? "#FF0000" : "#32CD32",
+    };
+
+    return (
+      <div
+        className="card"
+        onClick={() => setTurned(!turned)}
+        style={cardStyle}
+      >
+        {turned ? <h3>{answer}</h3> : <h3>{question}</h3>}
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mycontainer">
+      {qa.map((object) => (
+        <Flashcards question={object.question} answer={object.answer} key={object.id}/>
+      ))}
     </div>
   );
 }
