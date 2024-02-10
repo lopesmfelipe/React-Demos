0db9bab95d0a18d111ddb8f3ca774ab52e456e00
Felipe Lopes <felipemlopes0009@gmail.com>
2024-02-10 00:36:55 -0300

    Calculating Statistics

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -30,7 +30,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -50,7 +50,6 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     onAddItems(newItem);
 
@@ -119,10 +118,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 //Stats.js
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Add some items to your packing list🧑‍🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
-    <footer>
-      Your have X items on your list, and you already packed X(X%)
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go! ✈️"
+          : `💼 Your have ${numItems} items on your list,
+        and you already packed ${numPacked} ($
+        {percentage}%)`}
+      </em>
     </footer>
   );
 }
